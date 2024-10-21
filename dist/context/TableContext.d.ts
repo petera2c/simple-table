afb4861816ec39e5ec5a3147c6d3f43308a0e5ef
peter <20213436+petera2c@users.noreply.github.com>
2024-10-20 23:10:52 -0500

    New build

--- dist/context/TableContext.d.ts
+++ dist/context/TableContext.d.ts
@@ -1,8 +1,11 @@
 /// <reference types="react" />
 import CellValue from "../types/CellValue";
-declare const TableContext: import("react").Context<
-  {
-    [key: string]: CellValue;
-  }[]
->;
+declare const TableContext: import("react").Context<{
+    rows: {
+        [key: string]: CellValue;
+    }[];
+    tableRows: {
+        [key: string]: CellValue;
+    }[];
+}>;
 export default TableContext;
