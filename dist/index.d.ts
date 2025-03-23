78d07da53535a5020e60aa47b90fa5f4dcfd6a4a
peter <20213436+petera2c@users.noreply.github.com>
2025-03-23 16:23:59 -0500

    export Row and HeaderObject types

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,5 @@
 import SimpleTable from "./components/SimpleTable/SimpleTable";
+import type Row from "./types/Row";
+import type HeaderObject from "./types/HeaderObject";
 export { SimpleTable };
+export type { Row, HeaderObject };
--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,6 @@
 import SimpleTable from "./components/SimpleTable/SimpleTable";
+import type Row from "./types/Row";
+import type HeaderObject from "./types/HeaderObject";
 
 export { SimpleTable };
+export type { Row, HeaderObject };
