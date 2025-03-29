80b196c4c90db5e03b718a47241a466a4df4a715
peter <20213436+petera2c@users.noreply.github.com>
2025-03-29 09:43:24 -0500

    Icon polish

--- src/icons/AngleDownIcon.tsx
+++ src/icons/AngleDownIcon.tsx
@@ -1,11 +1,5 @@
 const AngleDownIcon = ({ className }: { className?: string }) => (
-  <svg
-    className={className}
-    viewBox="0 0 20 20"
-    width="20"
-    height="20"
-    xmlns="http://www.w3.org/2000/svg"
-  >
+  <svg className={className} viewBox="0 0 24 24" width="24" height="24" xmlns="http://www.w3.org/2000/svg">
     <path d="M5.41 7.59L10 12.17l4.59-4.58L16 9l-6 6-6-6z" />
   </svg>
 );
