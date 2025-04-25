f8a2767e23ce064fc98b66d67837c683318db184
peter <20213436+petera2c@users.noreply.github.com>
2025-04-25 18:59:37 -0500

    window fix

--- src/stories/examples/sales-example/SalesExample.tsx
+++ src/stories/examples/sales-example/SalesExample.tsx
@@ -2,7 +2,6 @@
 
 import { SALES_HEADERS } from "./sales-headers";
 import data from "./sales-data.json";
-import "simple-table-core/styles.css";
 import { Row, SimpleTable } from "../../..";
 
 export const SalesExampleComponent = ({ height = "70dvh" }: { height?: string }) => {
