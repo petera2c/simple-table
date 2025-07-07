54c35c5d9e03c52708b84f46a0c9b72214599295
peter <20213436+petera2c@users.noreply.github.com>
2025-07-06 23:07:42 -0500

    Basic example changes

--- src/components/simple-table/SimpleTable.tsx
+++ src/components/simple-table/SimpleTable.tsx
@@ -137,7 +137,6 @@ const SimpleTableComp = ({
   useOddColumnBackground = false,
 }: SimpleTableProps) => {
   if (useOddColumnBackground) useOddEvenRowBackground = false;
-  allowAnimations = true;
 
   // Force update function - needed early for header updates
   const [, forceUpdate] = useReducer((x) => x + 1, 0);
