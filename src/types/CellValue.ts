aff4eba16bc85884d4d80a108f9c77af0f9c3bf1
peter <20213436+petera2c@users.noreply.github.com>
2025-07-10 19:47:45 -0500

    Allow arrays for CellValue

--- src/types/CellValue.ts
+++ src/types/CellValue.ts
@@ -1,3 +1,11 @@
-type CellValue = string | number | boolean | undefined | null;
+type CellValue =
+  | string
+  | number
+  | boolean
+  | undefined
+  | null
+  | string[]
+  | number[]
+  | Record<string, any>[];
 
 export default CellValue;
