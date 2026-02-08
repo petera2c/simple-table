2733ee967405c699b253ab2a40c6b9cbc3db47ae
peter <20213436+petera2c@users.noreply.github.com>
2026-02-07 23:30:05 -0500

    csv export tests

--- src/components/simple-table/TableRow.tsx
+++ src/components/simple-table/TableRow.tsx
@@ -55,7 +55,6 @@ const TableRow = ({
     isRowSelected,
     loadingStateRenderer,
     maxHeaderDepth,
-    rows,
     useHoverRowBackground,
     useOddEvenRowBackground,
   } = useTableContext();
