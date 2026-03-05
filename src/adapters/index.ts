a71a3ab219f2d9082694afdda775ee8dbe94a424
Peter Young <20213436+petera2c@users.noreply.github.com>
2026-03-04 19:18:02 -0500

    Removing unnecessary changes

--- src/adapters/index.ts
+++
@@ -1,4 +0,0 @@
-export { SimpleTableAdapter } from "./react/SimpleTableAdapter";
-export type { SimpleTableAdapterProps } from "./react/SimpleTableAdapter";
-
-export { VanillaTable } from "./vanilla/VanillaTableExample";
